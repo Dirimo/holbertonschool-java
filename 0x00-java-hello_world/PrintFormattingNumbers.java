17e19a7cfe7715f2813e90b7012cac8a5b221e73
Dirimo <dirimoiriarte@gmail.com>
2025-11-07 19:41:26 +0100

    Last task

--- 0x00-java-hello_world/PrintFormattingNumbers.java
+++ 0x00-java-hello_world/PrintFormattingNumbers.java
@@ -2,6 +2,7 @@
         public static void main(String[] args) {
             float tax = 0.2456f;
             float value = 7654.321f;
-            System.out.println("Total: $" + String.format("%.2f", value) + "\nTax: " + String.format("%.3f", tax));
+            System.out.printf("Value: $%,.2f%n", value);
+            System.out.printf("Tax: %.2f%%%n", tax);
         }
     }
